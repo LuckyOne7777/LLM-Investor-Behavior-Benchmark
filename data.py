5c67d0bc743cf2c3f50083e230c69627d705aaeb
Nathan Smith <nathanbsmith729@gmail.com>
2025-12-11 00:02:36 +0000

    put constant variables at top of data.py file

--- data.py
+++ data.py
@@ -2,8 +2,14 @@ import pandas as pd
 from pathlib import Path
 import json
 import re 
+
+DAILY_UPDATES_FILE_NAME = Path(f"Daily Update - {pd.Timestamp.now().date()}.txt")
+PORTFOLIO_FILE_NAME = f"portfolio.csv"
+DEEP_RESEARCH_FILE_NAME = Path(f"Deep Research - {pd.Timestamp.now().date()}.txt")
+ORDERS_FILE_NAME = f"pending_trades.csv"
+
 def portfolio_exists(model_name: str):
-    PORTFOLIO_FILE_NAME = f"portfolio.csv"
+
     path = Path(f"models/{model_name}") / PORTFOLIO_FILE_NAME
     try:
         portfolio = pd.read_csv(path)
@@ -15,7 +21,6 @@ def portfolio_exists(model_name: str):
         return(False)
 def save_deep_research(txt: str, model: str):
 
-    DEEP_RESEARCH_FILE_NAME = Path(f"Deep Research - {pd.Timestamp.now().date()}.txt")
     path_to_deep_research = Path(f"models/{model}/Research/Deep Research")
     full_path = path_to_deep_research / DEEP_RESEARCH_FILE_NAME
     file = open(full_path, "w")
@@ -24,7 +29,6 @@ def save_deep_research(txt: str, model: str):
     return
 def save_daily_updates(txt: str, model: str):
 
-    DAILY_UPDATES_FILE_NAME = Path(f"Daily Update - {pd.Timestamp.now().date()}.txt")
     path_to_daily_updates = Path(f"models/{model}/Research/Daily Updates")
     full_path = path_to_daily_updates / DAILY_UPDATES_FILE_NAME
     file = open(full_path, "w")
@@ -48,7 +52,6 @@ def parse_orders_json(text: str):
     return json.loads(json_str)
 
 def save_orders(json_str: str, model_name: str):
-    ORDERS_FILE_NAME = f"pending_trades.csv"
     path = Path(f"models/{model_name}") / ORDERS_FILE_NAME
     f = open(path, "w")
     f.write(json_str)
@@ -56,12 +59,10 @@ def save_orders(json_str: str, model_name: str):
     return
 
 def delete_orders(model_name: str):
-    ORDERS_FILE_NAME = f"pending_trades.csv"
     path = Path(f"models/{model_name}") / ORDERS_FILE_NAME
     f = open(path, "w")
     f.close()
     return
 def load_orders(model_name: str):
-    ORDERS_FILE_NAME = f"pending_trades.csv"
     path = Path(f"models/{model_name}") / ORDERS_FILE_NAME
     return pd.read_csv(path)
