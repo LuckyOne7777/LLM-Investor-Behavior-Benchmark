fc28fdd621307f9974b409bfa176808d983d43a7
Nathan Smith <nathanbsmith729@gmail.com>
2025-12-16 02:11:12 +0000

    remove printing API key

--- backend/prompting/prompt_models.py
+++ backend/prompting/prompt_models.py
@@ -9,16 +9,16 @@ def prompt_orchestration(model_path, text):
         prompt_chatgpt(text)
 
 def prompt_deepseek(text):
-    print(os.getenv("DEEPSEEK_API_KEY"))
     client = OpenAI(api_key=os.environ.get("DEEPSEEK_API_KEY"),
                     base_url="https://api.deepseek.com")
     response = client.chat.completions.create(
-        model="deepseek-chat", messages={"role": "user", "content": text}, temperature=0.0,
+        model="deepseek-chat", messages=[{"role": "user", "content": text}], temperature=0.0,
         stream=False)
     return response.choices[0].message.content
 
 
 response = prompt_deepseek("respond exactly 'yes'.")
+print(response)
     
 def prompt_chatgpt(text):
     pass
