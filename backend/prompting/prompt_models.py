73aadee79e090541a91d31799d4034e37dbbd33d
Nathan Smith <nathanbsmith729@gmail.com>
2025-12-16 02:07:43 +0000

    finish prompt_deepseek

--- backend/prompting/prompt_models.py
+++ backend/prompting/prompt_models.py
@@ -1,6 +1,24 @@
-def prompt_orchestration(model, text):
-    pass
-def prompt_deepseek_model(text):
-    pass
+from openai import OpenAI
+import os
+
+def prompt_orchestration(model_path, text):
+    model = model_path.replace("models/")
+    if model == "deepseek":
+        prompt_deepseek(text)
+    elif model == "gpt-5":
+        prompt_chatgpt(text)
+
+def prompt_deepseek(text):
+    print(os.getenv("DEEPSEEK_API_KEY"))
+    client = OpenAI(api_key=os.environ.get("DEEPSEEK_API_KEY"),
+                    base_url="https://api.deepseek.com")
+    response = client.chat.completions.create(
+        model="deepseek-chat", messages={"role": "user", "content": text}, temperature=0.0,
+        stream=False)
+    return response.choices[0].message.content
+
+
+response = prompt_deepseek("respond exactly 'yes'.")
+    
 def prompt_chatgpt(text):
     pass
