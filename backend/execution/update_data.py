a388c93eaa144872db6a6d6254831c3aed7e2c51
Nathan Smith <nathanbsmith729@gmail.com>
2025-12-21 16:26:02 +0000

    delete gitignore clone

--- backend/execution/update_data.py
+++ backend/execution/update_data.py
@@ -2,10 +2,12 @@ import pandas as pd
 import yfinance as yf
 from datetime import date
 
+#TODO: ticker range fails on weekends
 def get_market_data(ticker):
         yesterdays_market_date = pd.Timestamp.now().date()
         todays_market_date = pd.Timestamp.now().date() + pd.Timedelta(days=1)
-        ticker_data = yf.download(ticker, start=yesterdays_market_date, end=todays_market_date, auto_adjust=True, )
+        ticker_data = yf.download(ticker, start=yesterdays_market_date, end=todays_market_date, auto_adjust=True)
+        ticker_data.iloc[-1]
         return ticker_data
 
 def update_market_value_column(portfolio):
