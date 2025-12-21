196df69ca73daa5d016da2dd8632879bb1187c74
Nathan Smith <nathanbsmith729@gmail.com>
2025-12-21 00:17:42 +0000

    create function for updating market vaalue

--- backend/execution/update_data.py
+++ backend/execution/update_data.py
@@ -5,6 +5,19 @@ from datetime import date
 def get_market_data(ticker):
         yesterdays_market_date = pd.Timestamp.now().date()
         todays_market_date = pd.Timestamp.now().date() + pd.Timedelta(days=1)
-        # for stock in self.portfolio["ticker"]:
         ticker_data = yf.download(ticker, start=yesterdays_market_date, end=todays_market_date, auto_adjust=True, )
         return ticker_data
+
+def update_market_value_column(portfolio):
+    portfolio = portfolio.copy()
+
+    for i, row in portfolio.iterrows():
+        ticker = row["ticker"]
+        shares = row["shares"]
+
+        ticker_data = get_market_data(ticker)
+        close_price = ticker_data["Close"]
+
+        portfolio.at[i, "market_value"] = close_price * shares
+
+    return portfolio
