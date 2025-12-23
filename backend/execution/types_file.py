266c19ebb0a27fd9f13ece9ff6f2310a2609f2d0
NAthan <nathanbsmith729@gmail.com>
2025-12-23 16:48:38 -0600

    Correct format for 'action'

--- backend/execution/types_file.py
+++ backend/execution/types_file.py
@@ -1,7 +1,7 @@
 from typing import TypedDict, Literal, Optional
 
 class Order(TypedDict):
-    action: Literal["buy", "sell", "u"]     # "u" = update stop-loss
+    action: Literal["b", "s", "u"]     # "u" = update stop-loss
     ticker: str
     shares: int
     order_type: Literal["limit", "market", "update"]
