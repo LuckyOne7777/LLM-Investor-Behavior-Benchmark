30cc17ae6465b3bb6be9dbd1050bd28ec7d6fbe5
NAthan <nathanbsmith729@gmail.com>
2025-12-23 14:43:21 -0600

    refactor get_market_data

--- backend/execution/update_data.py
+++ backend/execution/update_data.py
@@ -6,8 +6,17 @@ from datetime import date
 def get_market_data(ticker):
         yesterdays_market_date = pd.Timestamp.now().date()
         todays_market_date = pd.Timestamp.now().date() + pd.Timedelta(days=1)
-        ticker_data = yf.download(ticker, start=yesterdays_market_date, end=todays_market_date, auto_adjust=True)
-        ticker_data.iloc[-1]
+        ticker_data = yf.download(ticker, start=yesterdays_market_date, end=todays_market_date, auto_adjust=True, progress=False)
+        if isinstance(ticker_data.columns, pd.MultiIndex):
+             ticker_data.columns = ticker_data.columns.get_level_values(0)
+        print(type(ticker_data))
+        ticker_data = {
+            "Low": float(ticker_data["Low"].iloc[0]),
+            "High": float(ticker_data["High"].iloc[0]),
+            "Close": float(ticker_data["Close"].iloc[0]),
+            "Open": float(ticker_data["Open"].iloc[0]),
+            "Volume": int(ticker_data["Volume"].iloc[0])
+        }
         return ticker_data
 
 def update_market_value_column(portfolio):
