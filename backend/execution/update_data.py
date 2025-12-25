38046193061737c03afe84390a527b46b5ae8bd6
NAthan <nathanbsmith729@gmail.com>
2025-12-25 13:39:57 -0600

    Add explict types for returned dict

--- backend/execution/update_data.py
+++ backend/execution/update_data.py
@@ -1,25 +1,39 @@
 import pandas as pd 
 import yfinance as yf
+from typing import TypedDict
 
 #TODO: ticker range fails on weekends
 #TODO: graceful error handling for ticker downloading
-def get_market_data(ticker: str) -> dict:
+#TODO: Add additional data sources
+
+class MarketDataObject(TypedDict):
+     Low: float
+     High: float
+     Close: float
+     Open: float
+     Volume: int
+     Ticker: str
+
+def get_market_data(ticker: str) -> MarketDataObject:
         yesterdays_market_date = pd.Timestamp.now().date()
         todays_market_date = pd.Timestamp.now().date() + pd.Timedelta(days=1)
         try:
             ticker_data = yf.download(ticker, start=yesterdays_market_date, end=todays_market_date, auto_adjust=True, progress=False)
+            if ticker_data is None:
+                raise RuntimeError(f"YahooFinance API returned None for {ticker}. Try running again.")
         except Exception as e:
-             raise RuntimeError(f"Error downloading {ticker}'s data: {e}")
+             raise RuntimeError(f"Error downloading {ticker}'s data: {e}. Try running again.")
         if isinstance(ticker_data.columns, pd.MultiIndex):
              ticker_data.columns = ticker_data.columns.get_level_values(0)
-        ticker_data = {
+        data: MarketDataObject = {
             "Low": float(ticker_data["Low"].iloc[0]),
             "High": float(ticker_data["High"].iloc[0]),
             "Close": float(ticker_data["Close"].iloc[0]),
             "Open": float(ticker_data["Open"].iloc[0]),
-            "Volume": int(ticker_data["Volume"].iloc[0])
+            "Volume": int(ticker_data["Volume"].iloc[0]),
+            "Ticker": str(ticker)
         }
-        return ticker_data
+        return data
 
 def update_market_value_column(portfolio: pd.DataFrame) -> pd.DataFrame:
     portfolio = portfolio.copy()
