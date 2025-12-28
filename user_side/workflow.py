52b3276a5c813d6ff0ba33096228ffaec79d6dc5
NAthan <nathanbsmith729@gmail.com>
2025-12-27 23:01:15 -0600

    Put parse.py into backend

--- user_side/workflow.py
+++ user_side/workflow.py
@@ -1,6 +1,6 @@
 from backend.LIBB import LIBBmodel
 from .prompt_models import prompt_daily_report, prompt_deep_research
-from .parse import parse_json
+from backend.other.parse import parse_json
 import pandas as pd
 
 MODELS = ["deepseek", "gpt-4.1"]
