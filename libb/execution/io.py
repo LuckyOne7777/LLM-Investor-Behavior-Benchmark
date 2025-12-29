b70f36aac13b91fe6b1202f1b391822b8734d3b1
NAthan <nathanbsmith729@gmail.com>
2025-12-29 10:49:36 -0600

    Refactor catch_missing_order_data

--- libb/execution/io.py
+++ libb/execution/io.py
@@ -16,26 +16,25 @@ def append_log(path: Path, row: dict) -> None:
     df.to_csv(path, index=False)
 
 def catch_missing_order_data(order: Order, required_cols: list, trade_log_path: Path) -> bool:
-    """Log failures for missing or null required order data. Return True if needed data is in order."""
-    action = order["action"]
-    if action == "b":
-        action = "BUY"
-    elif action == "s":
-        action = "SELL"
-    elif action == "u":
-        action = "UPDATE"
+    """Log failures for missing or null data required for order.
+    Return True if needed data exists; False otherwise.
+    """
+    action_map = {"b": "BUY", "s": "SELL", "u": "UPDATE"}
+    action = action_map.get(order["action"], order["action"])
+
     missing_cols = []
     for col in required_cols:
         if col not in order or order[col] is None:
             missing_cols.append(col)
+
     if missing_cols:
         append_log(trade_log_path, {
-                "Date": order["date"],
-                "Ticker": order["ticker"],
-                "Action": action,
-                "Status": "FAILED",
-                "Reason": f"MISSING OR NULL ORDER INFO: {missing_cols}"
-            })
+            "Date": order["date"],
+            "Ticker": order["ticker"],
+            "Action": action,
+            "Status": "FAILED",
+            "Reason": f"MISSING OR NULL ORDER INFO: {missing_cols}"
+        })
         return False
-    else:
-        return True
+
+    return True
