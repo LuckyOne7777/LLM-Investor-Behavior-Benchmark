b7bd4cb59d9d4221002761f164f25b73b73990b1
NAthan <nathanbsmith729@gmail.com>
2026-02-01 19:36:44 -0600

    Create Log dataclass

--- libb/other/types_file.py
+++ libb/other/types_file.py
@@ -67,3 +67,19 @@ class MarketConfig:
             alpha_vantage_key=os.getenv("ALPHA_VANTAGE_KEY"),
             finnhub_key=os.getenv("FINNHUB_API_KEY"),
         )
+    
+@dataclass(slots=True)
+class Log:
+    date: str
+    weekday: str
+    started_at: str
+    finished_at: str
+    nyse_open_on_date: bool
+    created_after_close: bool
+    eligible_for_execution: bool
+    processing_status: str
+    orders_processed: int
+    orders_failed: int
+    orders_skipped: int
+    portfolio_value: float
+    error: str | Exception | None = None
