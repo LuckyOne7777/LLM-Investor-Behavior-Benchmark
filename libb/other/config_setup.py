4201341a908062e2057e3a6afe64f6aa0fca3b2c
NAthan <nathanbsmith729@gmail.com>
2026-03-18 09:51:41 -0500

    Create `config_setup.py`

---
+++ libb/other/config_setup.py
@@ -0,0 +1,30 @@
+_CONFIG_TYPES = {
+    "risk_free_rate": (float, int),
+    "trading_days_per_year": int,
+    "starting_cash": (float, int),
+    "slippage_pct_per_trade": (float, int),
+}
+
+_DEFAULT_CONFIG = {
+    "risk_free_rate": 0.045,
+    "trading_days_per_year": 252,
+    "starting_cash": 10_000,
+    "slippage_pct_per_trade": 0.0,
+}
+
+def _is_valid_type(key: str, value) -> bool:
+    return isinstance(value, _CONFIG_TYPES[key])
+
+def load_config(config: dict | None) -> dict:
+    if config is None:
+        return _DEFAULT_CONFIG.copy()
+
+    verified = {}
+    for key, default_value in _DEFAULT_CONFIG.items():
+        custom_value = config.get(key)
+        if custom_value is not None and _is_valid_type(key, custom_value):
+            verified[key] = custom_value
+        else:
+            verified[key] = default_value
+
+    return verified
